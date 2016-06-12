06df9a1369572abb314a22080038d20cb47d1cf5
MaRaPi <maxmania@web.de>
2016-06-12 11:04:58 +0200

    Update de.php
    little improvements

--- web/langs/de.php
+++ web/langs/de.php
@@ -1,45 +1,45 @@
 <?php
 
 define("HEADER_TITLE", "Computer-Forensik-System Coex");
-define("MENU_CAPABILITIES", "Capabilities");
-define("MENU_INSTALL", "Installieren");
+define("MENU_CAPABILITIES", "Einsatzmöglichkeiten");
+define("MENU_INSTALL", "Installieren"); //OR Installation
 define("MENU_ARCHITECTURE", "Architektur");
-define("MENU_ABOUT", "Informationen über uns");
+define("MENU_ABOUT", "Über uns");
 define("ARCHITECTURE_OF_SYSTEM", "Systemarchitektur");
-define("NEWS_OF_PROJECT", "Nachrichten von Projekt");
-define("CAPABILITIES_INFO", "Unsere Software-System sammelt Informationen aus einer Vielzahl von Quellen auf dem Zielrechner.");
+define("NEWS_OF_PROJECT", "Neues über das Projekt");
+define("CAPABILITIES_INFO", "Unser Software-System sammelt Informationen aus einer Vielzahl von Quellen auf dem Zielcomputer.");
 define("COEX_WHAT", "Was ist die COEX-Software?");
-define("COEX_DESCRIPTION", "Die Software für Computer-Forensik im Rahmen von Gerichtsverfahren , und entwickelt, um die Sammlung von Informationen über den Verdächtigen Computer zu erleichtern.");
+define("COEX_DESCRIPTION", "Die Software für Computer-Forensik im Rahmen von Gerichtsverfahren. Entwickelt um das Sammeln von Informationen über den verdächtigen Computer zu erleichtern.");
 define("DOWNLOAD_AND_RUN", "Herunterladen und ausführen.");
-define("CLONE_AND_BUILD", "Clone-Repository und kompilieren.");
+define("CLONE_AND_BUILD", "Repository klonen und kompilieren.");
 define("COEX_CHROME_MODULE", "Chrome-Modul");
-define("COEX_CHROME_MODULE_DESCRIPTION", "Sammelt die Passwort-Datenbank, Anwesenheitsprotokoll, um eine Liste der Erweiterungen und Browser-Einstellungen.");
+define("COEX_CHROME_MODULE_DESCRIPTION", "Sammelt Informationen über die Passwort-Datenbank, das Anwesenheitsprotokoll und eine Liste der Browser-Erweiterungen und Browser-Einstellungen.");
 define("COEX_IMAGEVERIFIER_MODULE", "Bild Verifier-Modul");
-define("COEX_IMAGEVERIFIER_MODULE_DESCRIPTION", "Sucht nach Bildern und überprüfen Sie sie auf mögliche Match-stego Behälter.");
+define("COEX_IMAGEVERIFIER_MODULE_DESCRIPTION", "Sucht nach Bildern und überprüfen Sie auf mögliche steganographische Container.");
 define("COEX_SEARCHPROGRAM_MODULE", "Suchen Programm-Modul");
-define("COEX_SEARCHPROGRAM_MODULE_DESCRIPTION", "Suchen Sie nach Spuren der installierten Programme, und in dem System gelöscht.");
+define("COEX_SEARCHPROGRAM_MODULE_DESCRIPTION", "Suchen Sie nach Spuren von im System gelöschter Software.");
 define("COEX_OUTLOOK_MODULE", "Outlook-Modul");
-define("COEX_OUTLOOK_MODULE_DESCRIPTION", "Er findet Korrespondenz und Kopien des Outlook Express Mail-Client.");
+define("COEX_OUTLOOK_MODULE_DESCRIPTION", "Sammelt Korrespondenz und Kopien des Outlook Express Mail-Client.");
 define("COEX_PIDGIN_MODULE", "Pidgin-Modul");
-define("COEX_PIDGIN_MODULE_DESCRIPTION", "Sammelt Benutzer Korrespondenz und eine Liste seiner Kontakte, Instant Messenger-Protokolle.");
+define("COEX_PIDGIN_MODULE_DESCRIPTION", "Sammelt Benutzer Korrespondenz, Kontaktliste und Instant Messenger-Protokolle.");
 define("COEX_ICQ_MODULE", "ICQ-Modul");
-define("COEX_ICQ_MODULE_DESCRIPTION", "Sammeln Sie eine Liste von Benutzer Messenger-Kontakte.");
+define("COEX_ICQ_MODULE_DESCRIPTION", "Sammelt die Benutzer Kontaktliste.");
 define("COEX_SYSTEMLOG_MODULE", "Der Systemprotokoll-Modul");
-define("COEX_SYSTEMLOG_MODULE_DESCRIPTION", "Es sammelt Protokolle Betriebssystem.");
+define("COEX_SYSTEMLOG_MODULE_DESCRIPTION", "Sammelt die Betriebssystem Protokolle.");
 define("COEX_FIREFOX_MODULE", "Firefox-Modul");
-define("COEX_FIREFOX_MODULE_DESCRIPTION", "Sammelt Geschichte gleichnamigen Browser durchsuchen.");
+define("COEX_FIREFOX_MODULE_DESCRIPTION", "Sammelt den Browserverlauf.");
 define("COEX_ARCHIVE_MODULE", "Archiv-Modul");
-define("COEX_ARCHIVE_MODULE_DESCRIPTION", "Suchen nach Dateien und zeigt Informationen über sie.");
-define("COEX_SEARCH_GIT_REPO_MODULE", "SearchGitRepo модуль");
+define("COEX_ARCHIVE_MODULE_DESCRIPTION", "Sucht nach Dateien und zeigt deren Informationen.");
+define("COEX_SEARCH_GIT_REPO_MODULE", "SearchGitRepo модуль"); //no chance bro ;)
 define("COEX_SEARCH_GIT_REPO_MODULE_DESCRIPTION", "Осуществляет поиск Git-репозиториев и выводит информацию о них.");
-define("ARCHITECTURE_DESCRIPTION", "Die Modularität des gesamten Systems ermöglicht es, unter den vorgeschriebenen Aufgaben schnell zu konfigurieren. Jedes Modul arbeitet unabhängig von den anderen, das Ergebnis am Ende des Systems in einem einzigen Bericht antreibt.");
-define("ARCHITECTURE_SYSTEM_OF_PLUGINS", "Stecksystem");
+define("ARCHITECTURE_DESCRIPTION", "Durch die Modularität des gesamten Systems kann die Software für die anfallenden Aufgaben schnell konfiguriert werden. Jedes Modul arbeitet unabhängig von den anderen. Das Ergebnis wird am Ende in einem einzigen Bericht zusammengefasst.");
+define("ARCHITECTURE_SYSTEM_OF_PLUGINS", "Plug-in");
 define("ARCHITECTURE_DATA_ANALISIS", "Datenanalyse");
 define("ARCHITECTURE_MODUL_N", "Modul");
-define("CONTACTS", "Kontakte");
-define("CONTACTS_DESCRIPTION", "Um uns zu kontaktieren, verwenden Sie bitte die Tools unten vorgeschlagen.");
+define("CONTACTS", "Kontakt");
+define("CONTACTS_DESCRIPTION", "Verwenden Sie bitte die vorgeschlagen Tools für eine Kontaktaufnahme.");
 define("ABOUT_US", "Über uns");
-define("MADE_IN", "Hergestellt auf einem Stuhl in KIBEVS TUSUR GAP-Gruppe.");
-define("DENIAL_OF_RESPONSIBILITY", "Denial-of-Verantwortung");
-define("DENIAL_OF_RESPONSIBILITY_DESCRIPTION", "Die verwendeten Medien wurden aus dem Internet, die richtigen Daten-Dateien sind Eigentum ihrer rechtmäßigen Eigentümer.");
+define("MADE_IN", "Hergestellt in der Abteilung KIBEVS TUSUR der GAP-Gruppe."); // please check the name and the englisch version
+define("DENIAL_OF_RESPONSIBILITY", "Urheberrecht");
+define("DENIAL_OF_RESPONSIBILITY_DESCRIPTION", "Die verwendeten Medien/Bilder stammen aus dem Internet. Sie sind Eigentum ihrer rechtmäßigen Eigentümer.");//no garanty for law-security
 define("LICENSE_AGREEMENT", "Lizenzvereinbarung");
